5b1e878be7b36bc032185158d1163daedb2fa827
Sho Ishiguro <ishiguro.sho.62u@st.kyoto-u.ac.jp>
2020-10-05 01:56:28 +0900

    Replace the previous one with the correct one
    Y axis range, position numbers, and aesthetics of the plot are fixed.

--- Misc/serial_position_curve_check.R
+++ Misc/serial_position_curve_check.R
@@ -1,12 +1,15 @@
 library(dplyr)
 
-data <- read.csv('../Data/data_for_analysis.csv',
+data <- read.csv('../Data/behav_data.csv',
                       row.names=1)
+
+data$position <- data$position + 1
 
 # Check U-shaped performance with a strong primacy effect
 pdf("../Results/Serial_Position_Curve.pdf")
 data %>%
   group_by(position) %>%
   summarize(Correct_Recall = mean(correct_in_position, na.rm =TRUE)) %>%
-  plot(type="o", ylab = "Correct Recall", xlab = "Serial Position")
+  plot(type="o", pch=19, bty = 'l',ylim = c(0.0, 1.0),
+       ylab = "Correct Recall", xlab = "Serial Position")
 dev.off()
